87f849b56402fcfd8d3a5fc0068856ed578c5f52
최지우 <andyandy0409@naver.com>
2024-10-27 15:35:08 +0900

    feat: #7 뚜벗 프로필 수정

--- client/src/styles/TtubeotProfile.tsx
+++ client/src/styles/TtubeotProfile.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import {Image, Text, View} from 'react-native';
+import {Shadow} from 'react-native-shadow-2';
 import Icon from '../components/Icon';
+
+const Profile = styled.View`
+  position: relative;
+  width: 275px;
+  height: 85px;
+  display: 'flex';
+  flex-direction: row;
+`;
 
 const ProfileImageContainer = styled.View`
   position: absolute;
-  right: 0;
+  left: -75px;
   width: 80px;
   height: 80px;
   border-radius: 50px;
@@ -20,38 +29,70 @@ const ProfileImage = styled(Image)`
 `;
 
 const ProfileDetails = styled.View`
-  position: absolute;
-  left: -15px;
   background-color: #b1baaa;
   border-radius: 20px;
-  padding-top: 10px;
   padding-left: 15px;
-  padding-bottom: 15px;
   flex-direction: column;
+  justify-content: center;
   width: 200px;
+`;
+
+const ProfileTop = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+  margin-left: 5px;
+  position: relative;
 `;
 
 const ProfileName = styled(Text)`
   font-size: 18px;
   font-weight: bold;
   color: #fff;
-  margin-bottom: 7px;
-  margin-left: 5px;
 `;
 
-const ProfileInfo = styled(View)`
+const RemainDays = styled(Text)`
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  position: absolute;
+  top: 2px;
+  right: 20px;
+  background-color: rgba(96, 92, 80, 0.5);
+  border-radius: 15px;
+  padding: 2px 8px;
+`;
+
+const ProfileBottom = styled(View)`
   display: flex;
   flex-direction: row;
   gap: 5px;
   align-items: center;
+  position: relative;
+`;
+
+const WalkWith = styled(Text)`
+  position: absolute;
+  bottom: 2px;
+  right: 10px;
+  font-weight: bold;
+  color: #fff;
 `;
 
 const TtubeotProfile = () => {
   return (
-    <>
+    <Profile>
+      <ProfileImageContainer>
+        <ProfileImage source={require('../assets/ttubeot/mockTtu.png')} />
+      </ProfileImageContainer>
       <ProfileDetails>
-        <ProfileName>우리마루</ProfileName>
-        <ProfileInfo>
+        <ProfileTop>
+          <ProfileName>우리마루</ProfileName>
+          <RemainDays>D-4</RemainDays>
+        </ProfileTop>
+        <ProfileBottom>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>함께한 거리</Text>
           <View
             style={{
               backgroundColor: '#605C50',
@@ -61,15 +102,10 @@ const TtubeotProfile = () => {
             }}>
             <Icon type="FontAwesome5" name="paw" size={12} color="#000" />
           </View>
-          <Text style={{color: 'white', fontWeight: 'bold'}}>
-            함께한 거리 55,000 걸음
-          </Text>
-        </ProfileInfo>
+          <WalkWith>55,000 걸음</WalkWith>
+        </ProfileBottom>
       </ProfileDetails>
-      <ProfileImageContainer>
-        <ProfileImage source={require('../assets/ttubeot/mockTtu.png')} />
-      </ProfileImageContainer>
-    </>
+    </Profile>
   );
 };
 
