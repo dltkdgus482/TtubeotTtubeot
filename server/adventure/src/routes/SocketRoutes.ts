22eaa496387f539a6e609e0702d4aa9a3cc73636
김상범 <picel@picel.net>
2024-10-28 14:19:26 +0900

    feat: #16 disconnect handler 구체화

--- server/adventure/src/routes/SocketRoutes.ts
+++ server/adventure/src/routes/SocketRoutes.ts
@@ -17,8 +17,6 @@ export default function socketRoutes(io: Server) {
 
     socket.on("adventure_end", () => adventureController.handleEndAdventure(socket));
 
-    socket.on("disconnect", () => {
-      console.log("Client disconnected:", socket.id);
-    });
+    socket.on("disconnect", () => adventureController.handleDisconnect(socket));
   });
 }
