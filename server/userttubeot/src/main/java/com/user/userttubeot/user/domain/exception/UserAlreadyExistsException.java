23126ed81d84aeeda2976d348c4ac4e763894b7a
박연후 <qkrdusgn00@naver.com>
2024-10-28 11:10:19 +0900

    comment: #11 예외 주석 추가

--- server/userttubeot/src/main/java/com/user/userttubeot/user/domain/exception/UserAlreadyExistsException.java
+++ server/userttubeot/src/main/java/com/user/userttubeot/user/domain/exception/UserAlreadyExistsException.java
@@ -1,5 +1,8 @@
 package com.user.userttubeot.user.domain.exception;
 
+/**
+ * 이미 존재하는 사용자에 대한 예외 클래스. RuntimeException을 상속하여 사용자 정의 예외를 생성합니다.
+ */
 public class UserAlreadyExistsException extends RuntimeException {
 
     public UserAlreadyExistsException(String message) {
