aa2b1a1911432b6ed784e23da69d1327a3351985
박연후 <qkrdusgn00@naver.com>
2024-10-28 16:40:20 +0900

    feat: #11 리프레시 토큰 Redis 저장

--- server/userttubeot/src/main/java/com/user/userttubeot/user/infrastructure/security/JWTUtil.java
+++ server/userttubeot/src/main/java/com/user/userttubeot/user/infrastructure/security/JWTUtil.java
@@ -1,7 +1,9 @@
 package com.user.userttubeot.user.infrastructure.security;
 
+import com.user.userttubeot.user.application.RedisService;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class JWTUtil {
 
     private final SecretKey secretKey;
+    private final RedisService redisService;
 
-    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret, RedisService redisService) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
             Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.redisService = redisService; // 주입된 인스턴스 사용
     }
 
     public String getUserPhone(String token) {
@@ -40,12 +44,22 @@ public class JWTUtil {
 
     public String createRefreshToken(String userPhone) {
         long expiredMs = 1000 * 60 * 60 * 24; // 1일
-        return Jwts.builder()
+        String refreshToken = Jwts.builder()
             .claim("userPhone", userPhone)
             .issuedAt(new Date(System.currentTimeMillis()))
             .expiration(new Date(System.currentTimeMillis() + expiredMs))
             .signWith(secretKey)
             .compact();
+
+        String key = "refresh_" + userPhone;
+
+        if (redisService.getValue(key) != null) {
+            redisService.deleteValue(key);
+        }
+        // Redis에 리프레시 토큰 저장 (Duration으로 TTL 설정)
+        redisService.setValues(key, refreshToken, Duration.ofMillis(expiredMs));
+
+        return refreshToken;
     }
 
 }
