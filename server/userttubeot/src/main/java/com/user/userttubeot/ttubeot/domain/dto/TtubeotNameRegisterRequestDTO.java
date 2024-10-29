9cc76b6b68b4f3b29bc5a5ab1a5cc414925fcb97
선예림 <syl1602@naver.com>
2024-10-29 16:36:49 +0900

    feat: #21. 뚜벗 뽑기, 이름 등록에 필요한 DTO 작성

---
+++ server/userttubeot/src/main/java/com/user/userttubeot/ttubeot/domain/dto/TtubeotNameRegisterRequestDTO.java
@@ -0,0 +1,11 @@
+package com.user.userttubeot.ttubeot.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class TtubeotNameRegisterRequestDTO {
+
+    private Integer userTtubeotOwnershipId;
+    private String userTtubeotOwnershipName; // 등록할 뚜벗의 이름
+
+}
