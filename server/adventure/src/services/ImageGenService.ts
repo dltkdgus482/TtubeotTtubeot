fecb54c148d9063b2618afefbaad17fc6b678696
김상범 <picel@picel.net>
2024-10-31 16:40:41 +0900

    feat: #37 image to image AI API 연동

---
+++ server/adventure/src/services/ImageGenService.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import MonsterApiClient from "monsterapi";
+
+dotenv.config();
+
+class ImageGenService {
+  private apiKey: string;
+  private client: MonsterApiClient;
+
+  constructor() {
+    this.apiKey = process.env.MONSTER_API_KEY as string;
+    this.client = new MonsterApiClient(this.apiKey);
+  }
+
+  public async image2image(init_image_url: string): Promise<string> {
+    const prompt = `cartoon style, simplified, pastel tone, `;
+
+    const model = 'pix2pix';
+    const input = {
+      prompt,
+      init_image_url,
+      negprompt: "text, watermark, blurred, low resolution",
+      steps: 50,
+      guidance_scale: 30,
+      image_guidance_scale: 2,
+    };
+    console.log(init_image_url);
+
+    try {
+      const response = await this.client.generate(model, input);
+      console.log(response);
+      return response.output[0];
+    } catch (error) {
+      console.error("Error generating image:", error);
+      return "";
+    }
+  }
+}
+
+export default ImageGenService;
