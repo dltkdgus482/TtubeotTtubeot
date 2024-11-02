69307d537026a8464c9ca5e0379c2cd9ce6467c7
차민주 <ckalswn007@naver.com>
2024-11-02 22:19:59 +0900

    feat: #19 회원가입 화면 구현 및 api 연동

--- client/src/components/Auth/TermsOfUseScreen.styles.ts
+++ client/src/components/Auth/TermsOfUseScreen.styles.ts
@@ -1,6 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   containerText: {
     color: '#3F3F3F',
     marginTop: 6,
