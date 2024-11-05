c3860e21a1465ece7a074e8eaf4ed96cd891f905
선예림 <syl1602@naver.com>
2024-11-05 17:08:10 +0900

    feat: #48. fcm 관련 user 테이블 fcm_token 필드 추가

--- server/userttubeot/src/main/java/com/user/userttubeot/user/domain/repository/UserRepository.java
+++ server/userttubeot/src/main/java/com/user/userttubeot/user/domain/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.user.userttubeot.user.domain.repository;
 
 import com.user.userttubeot.user.domain.entity.User;
+import jakarta.transaction.Transactional;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +17,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUserPhone(String userPhone);
 
     boolean existsByUserName(String userName);
+
+    Optional<User> findByUserId(Integer userId);
+
+    // fcmToken 업데이트
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.fcmToken = :fcmToken WHERE u.userId = :userId")
+    int updateFcmTokenByUserId(@Param("userId") Integer userId, @Param("fcmToken") String fcmToken);
+
 }
