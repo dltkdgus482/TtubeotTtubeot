2549993fd5fec3c8cfea948a3da3ac8085cd3248
박연후 <qkrdusgn00@naver.com>
2024-11-06 17:19:39 +0900

    refactor: 친구 태그 DTO JsonProperty 추가

--- server/userttubeot/src/main/java/com/user/userttubeot/user/domain/dto/FriendRequestDto.java
+++ server/userttubeot/src/main/java/com/user/userttubeot/user/domain/dto/FriendRequestDto.java
@@ -1,11 +1,14 @@
 package com.user.userttubeot.user.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class FriendRequestDto {
 
+    @JsonProperty("userId")
     private Integer userId;
+    @JsonProperty("friendId")
     private Integer friendId;
 
 }
