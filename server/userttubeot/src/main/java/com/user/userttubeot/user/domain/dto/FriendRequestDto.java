925ebb7328f7685a8d84702a33b3720d1c52b54a
선예림 <syl1602@naver.com>
2024-11-06 12:16:03 +0900

    Update FriendRequestDto.java

--- server/userttubeot/src/main/java/com/user/userttubeot/user/domain/dto/FriendRequestDto.java
+++ server/userttubeot/src/main/java/com/user/userttubeot/user/domain/dto/FriendRequestDto.java
@@ -6,9 +6,7 @@ import lombok.Data;
 @Data
 public class FriendRequestDto {
 
-    @JsonProperty("user_id")
     private Integer userId;
-    @JsonProperty("friend_id")
     private Integer friendId;
 
 }
