0ea07ef0ccb1f5d4c2de008c412f6190e6bbbbc6
박연후 <qkrdusgn00@naver.com>
2024-11-12 21:34:41 +0900

    style: 랭킹 스타일 수정

--- client/src/screens/Ranking/RankingScreen.tsx
+++ client/src/screens/Ranking/RankingScreen.tsx
@@ -6,7 +6,8 @@ import RankingScreenButtonContainer from './RankingScreenButtonContainer';
 import { dummyRankingList } from './dummyData';
 import { getRankingInfo } from '../../utils/apis/Ranking/getRankingInfo';
 import { useUser } from '../../store/user';
-import ttubeotImages from './imageMap';
+import { profileColor } from '../../components/ProfileImageUrl';
+import { useIsFocused } from '@react-navigation/native';
 // import { useIsFocused } from '@react-navigation/native';
 
 const IntroTtubeotRabbit = require('../../assets/ttubeot/IntroTtubeotRabbit.png');
@@ -25,6 +26,7 @@ interface RankingProps {
 const RankingScreen = () => {
   // const [selected, setSelected] = useState('뚜벗 랭킹');
   const [rankingList, setRankingList] = useState<RankingProps[]>([]);
+  const isFocused = useIsFocused(); // 화면 포커스 여부 감지
 
   useEffect(() => {
     const fetchRankingInfo = async () => {
@@ -36,41 +38,60 @@ const RankingScreen = () => {
       });
     };
 
-    fetchRankingInfo();
-  }, []);
+    if (isFocused) {
+      // 화면이 포커스되었을 때만 실행
+      fetchRankingInfo();
+    }
+  }, [isFocused]);
 
   return (
     <View style={styles.backGround}>
       <RankingScreenButtonContainer />
       <View style={styles.topThreeList}>
-        <View style={styles.second}>
-          <View style={styles.playerImageContainer}>
-            <Image source={ttubeotDog} style={styles.playerImage} />
+        {rankingList[1] && (
+          <View style={styles.second}>
+            <View style={styles.playerImageContainer}>
+              <Image
+                source={profileColor[rankingList[1].ttubeotId]}
+                style={(styles.playerImage, { width: 80, height: 80 })}
+              />
+            </View>
+            {/* <StyledText bold style={styles.playerName}>
+        {rankingList[1].username}
+      </StyledText> */}
+            <Image source={silver} style={styles.medal} />
           </View>
-          {/* <StyledText bold style={styles.playerName}>
-            {rankingList[1].username}
-          </StyledText> */}
-          <Image source={silver} style={styles.medal} />
-        </View>
-        <View style={styles.first}>
-          <View style={styles.playerImageContainer}>
-            <Image source={ttubeotDog} style={styles.playerImage} />
+        )}
+        {rankingList[0] && (
+          <View style={styles.first}>
+            <View style={styles.playerImageContainer}>
+              <Image
+                source={profileColor[rankingList[0].ttubeotId]}
+                style={(styles.playerImage, { width: 80, height: 80 })}
+              />
+            </View>
+            {/* <StyledText bold style={styles.playerName}>
+        {rankingList[0].username}
+      </StyledText> */}
+            <Image source={gold} style={styles.medal} />
           </View>
-          {/* <StyledText bold style={styles.playerName}>
-            {rankingList[0].username}
-          </StyledText> */}
-          <Image source={gold} style={styles.medal} />
-        </View>
-        <View style={styles.third}>
-          <View style={styles.playerImageContainer}>
-            <Image source={ttubeotDog} style={styles.playerImage} />
+        )}
+        {rankingList[2] && (
+          <View style={styles.third}>
+            <View style={styles.playerImageContainer}>
+              <Image
+                source={profileColor[rankingList[2].ttubeotId]}
+                style={(styles.playerImage, { width: 80, height: 80 })}
+              />
+            </View>
+            {/* <StyledText bold style={styles.playerName}>
+        {rankingList[2].username}
+      </StyledText> */}
+            <Image source={bronze} style={styles.medal} />
           </View>
-          {/* <StyledText bold style={styles.playerName}>
-            {rankingList[2].username}
-          </StyledText> */}
-          <Image source={bronze} style={styles.medal} />
-        </View>
+        )}
       </View>
+
       <ScrollView
         style={styles.rankingList}
         contentContainerStyle={{ paddingBottom: 120 }}>
@@ -84,8 +105,8 @@ const RankingScreen = () => {
               </StyledText>
               <View style={styles.rankingInfo}>
                 <Image
-                  source={ttubeotImages[ranking.ttubeotId] || ttubeotImages[1]}
-                  style={styles.rankingImage}
+                  source={profileColor[ranking.ttubeotId] || profileColor[1]}
+                  style={(styles.playerImage, { width: 80, height: 80 })}
                 />
                 <StyledText bold style={styles.rankingName}>
                   {ranking.username}
