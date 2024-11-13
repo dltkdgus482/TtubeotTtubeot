19408c3be7931323817d1a764d78463b15369548
선예림 <syl1602@naver.com>
2024-11-13 09:38:10 +0900

    feat: 뚜벗의 관심도 조회 api 설계

---
+++ server/userttubeot/src/main/java/com/user/userttubeot/ttubeot/domain/dto/UserTtubeotInterestResponseDTO.java
@@ -0,0 +1,12 @@
+package com.user.userttubeot.ttubeot.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserTtubeotInterestResponseDTO {
+
+    private Integer ttubeotInterest;
+
+}
